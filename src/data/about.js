06a242d22146a19884809852a3bf22f87473887a
Menthy <wumengxuan0728@gmail.com>
2023-07-22 20:23:49 -0700

    about

---
+++ src/data/about.js
@@ -0,0 +1,6 @@
+import image from "../../public/image3.png";
+export const show = {
+  image: image,
+  text: "Maryland Masti is an annual intercollegiate Garba/Raas competition held by the students at the University of Maryland, College Park. Our goals include spreading awareness of the traditional folk dance cultures of the West Indian State of Gujarat, providing an outlet for current students to express their passion for such dance, entertaining the guests at our show, and having a great weekend -- staff, teams, and audience alike!",
+  title: "What is Maryland Masti",
+};

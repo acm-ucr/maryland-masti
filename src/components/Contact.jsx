dd44cb0c32ee25b46d6c4a278fc0dca103f43484
Divyank Shah <divyank.shah.2016@gmail.com>
2023-07-22 22:51:09 -0400

    contact working

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,20 @@
+import { FaSquareFacebook, FaInstagram, FaEnvelope } from "react-icons/fa6";
+import { links } from "@/data/links";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="bg-masti-red-200 flex items-center justify-center py-4">
+      <a href={links.facebook} target="_blank" rel="noreferrer">
+        <FaSquareFacebook className="text-white text-8xl mx-4 hover:!text-masti-beige hover:cursor-pointer" />
+      </a>
+      <a href={`mailto:${links.email}`} target="_blank" rel="noreferrer">
+        <FaEnvelope className="text-white text-8xl mx-4 hover:!text-masti-beige hover:cursor-pointer" />
+      </a>
+      <a href={links.instagram} target="_blank" rel="noreferrer">
+        <FaInstagram className="text-white text-8xl mx-4 hover:!text-masti-beige hover:cursor-pointer" />
+      </a>
+    </div>
+  );
 };
 
 export default Contact;
